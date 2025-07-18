2394f2f0444e6364161bb08a00365e6f848a22c6
mulkah-dev <mulkah.akala@gmail.com>
2025-07-18 23:23:37 +0100

    Add task 2

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from . import views
+from . import LibraryDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/', views.book_list),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
