8b93d548b683508f6bde47651a1cc9d33b794979
mulkah-dev <mulkah.akala@gmail.com>
2025-07-20 07:25:32 +0100

    Add changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,6 +13,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        permissions = [("can_add_book", "can_change_book", "can_delete_book")]
 
 class Library(models.Model):
     name = models.CharField(max_length = 100)
