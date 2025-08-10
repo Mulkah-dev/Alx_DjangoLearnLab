0826812e16f96ce0db62e19f016e945a2b043581
mulkah-dev <mulkah.akala@gmail.com>
2025-08-10 19:46:55 +0100

    Add task 2

---
+++ advanced-api-project/README.md
@@ -0,0 +1,7 @@
+### Book API Endpoints
+
+- `GET /api/books/` → List all books
+- `POST /api/books/` → Create new book (Auth required)
+- `GET /api/books/<id>/` → Retrieve single book
+- `PUT /api/books/<id>/` → Update book (Auth required)
+- `DELETE /api/books/<id>/` → Delete book (Auth required)
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+# ListView & CreateView combined
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    Handles listing all books (GET) and creating a new book (POST).
+    Read access is open, but creating requires authentication.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# DetailView, UpdateView & DeleteView combined
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles retrieving a single book (GET), updating it (PUT/PATCH), 
+    and deleting it (DELETE).
+    Updating and deleting require authentication.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
