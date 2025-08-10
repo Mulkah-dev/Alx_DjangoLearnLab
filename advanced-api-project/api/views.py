17e5cc1aa1c51b052fec825c4ea4fd9b0141d7cd
mulkah-dev <mulkah.akala@gmail.com>
2025-08-10 22:54:25 +0100

    Add updates

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 # ----- Generic CRUD Views with filtering, searching, and ordering -----
 
